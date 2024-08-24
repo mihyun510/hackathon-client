24695aa569c5de8fd08b5d860f41bc6adca62123
LENOVO-IDEAPAD-\a <azanghs@gmail.com>
2024-08-24 19:02:03 +0900

    최초 커밋

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+// App.tsx
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./components/shared";
+import {
+  CartPage,
+  HomePage,
+  NotFoundPage,
+  ProductCreatePage,
+  ProductPage,
+  PurchasePage,
+  LoginPage
+} from "./pages";
+import { AuthProvider } from "./contexts/AuthContext";  // AuthProvider 추가
+
+//수정화면.
+function App() {
+  return (
+    <AuthProvider>
+    <Layout>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="create" element={<ProductCreatePage />} />
+        <Route path="cart" element={<CartPage />} />
+        <Route path="login" element={<LoginPage/>} /> 
+        <Route path="product/:productId" element={<ProductPage />} />
+        <Route path="purchase/:productId" element={<PurchasePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Layout>
+    </AuthProvider>
+  );
+}
+
+export default App;
